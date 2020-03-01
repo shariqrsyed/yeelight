1d9bda7dfae6a79cf526170511821e31c0d080f6
Shariq Syed <shariq.r.syed@gmail.com>
2020-03-01 11:52:45 -0500

    Update

--- index.js
+++ index.js
@@ -1,28 +1,26 @@
 var schedule = require('node-schedule');
 var SunCalc = require('suncalc');
 const Lookup = require("node-yeelight-wifi").Lookup;
-
 var sunsetDate = new Date();
 
-schedule.scheduleJob('0 0 * * *', () => {
-  var times = SunCalc.getTimes(new Date(), 'long', 'lat');
-  sunsetDate = times.sunsetStart;
-})
-
-schedule.scheduleJob(sunsetDate, function(){
+var sunsetSchedule = schedule.scheduleJob('sunsetJob', sunsetDate, function(){
   let look = new Lookup();
 
   look.on("detected",(light) =>
   {
       console.log("new yeelight detected: id="+light.id);
       if(light){
-        light.setPower(true).then(() =>
-        {
-            console.log("success");
-        }).catch((error =>
-        {
-            console.log("failed",error);
-        }));
+        light.setPower(true).then(() =>{console.log("turned on the lights");})
+        .catch((error =>{console.log("failed to turn on the lights",error);}));
       }
   });
 });
+
+var midnight = schedule.scheduleJob('0 1 * * *', () => {
+  var times = SunCalc.getTimes(new Date(), 'long', 'lat');
+  sunsetDate = times.sunsetStart;
+  var sunsetJob = schedule.scheduledJobs['sunsetJob'];
+  var value = sunsetJob.reschedule(sunsetDate);
+  console.log('reschedule value: ' + value);
+  console.log('set sunset time for: ' + sunsetDate);
+})
